2f17f6acae5a18e3270769a942d3fe7b7da9ad7c
KokoT <prikolkillercat@gmail.com>
2024-09-09 13:12:38 +0200

    added tar archive creating

--- packer.py
+++ packer.py
@@ -2,6 +2,7 @@ import os
 import tarfile
 from config import *
 
+
 def pack(targetFolder:str, archive:str):
     programLogger.info(f'packing "{targetFolder}"; reading content...')
     files = []
@@ -14,6 +15,18 @@ def pack(targetFolder:str, archive:str):
             files.append(relative_path)
 
             packSize += os.path.getsize(os.path.join(targetFolder, relative_path))
+            scannedSize += os.path.getsize(os.path.join(targetFolder, relative_path))
+
     programLogger.info(f'reading success; total files: {len(files)} [{packSize}B/{scannedSize}B]; ignored total: {ignored}')
 
 
+    if DEBUG:
+        if not os.path.exists('./debug'): os.mkdir('./debug')
+        if not os.path.exists('./debug/tmp'): os.mkdir('./debug/tmp')
+        tmp = './debug/tmp'
+
+    programLogger.info(f'adding to archive...')
+    with tarfile.open(os.path.join(tmp, archive), 'x') as t:
+        for f in files:
+            t.add(os.path.join(targetFolder, f), arcname=f)
+    programLogger.info(f'success')
