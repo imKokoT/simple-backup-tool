d5e63b53bd320b2dac53bf079788949fc323ed1e
KokoT <prikolkillercat@gmail.com>
2024-09-20 12:35:49 +0200

    added base dearchive

--- archiver.py
+++ archiver.py
@@ -8,7 +8,7 @@ def archive(schemaName:str) -> str:
 
     if not schema:
         programLogger.fatal(f'failed to load schema name "{schemaName}"')
-        exit(0)
+        exit(1)
     
     match schema.get('compressFormat'):
         case '7z':
@@ -24,3 +24,36 @@ def archive(schemaName:str) -> str:
         case _:
             programLogger.fatal(f'unknown compression format {schema.get('compressionFormat', None)}')
             exit(1)        
+
+
+def dearchive(schemaName:str, schema:dict) -> str:
+    '''returns pack name'''
+    if DEBUG:
+        if not os.path.exists('./debug'): os.mkdir('./debug')
+        if not os.path.exists('./debug/tmp'): os.mkdir('./debug/tmp')
+        tmp = './debug/tmp'
+    else:
+        raise NotImplementedError()
+
+
+    downloaded = os.path.join(tmp, f'{schemaName}.downloaded')
+
+    match schema.get('compressFormat'):
+        case '7z':
+            raise NotImplementedError()
+        case 'gz':
+            raise NotImplementedError()
+        case 'bz':
+            raise NotImplementedError()
+        case 'zip':
+            raise NotImplementedError()
+        case None | 'tar':
+            if os.path.exists(os.path.join(tmp, f'{schemaName}.tar')):
+                os.remove(os.path.join(tmp, f'{schemaName}.tar'))
+
+            os.rename(downloaded, os.path.join(tmp, f'{schemaName}.tar'))
+            
+            return f'{schemaName}.tar'
+        case _:
+            programLogger.fatal(f'unknown compression format {schema.get('compressionFormat', None)}')
+            exit(1)
