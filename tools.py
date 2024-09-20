4b6166d7a057c795722f8a1ed37d3942720c8a77
KokoT <prikolkillercat@gmail.com>
2024-09-20 10:37:11 +0200

    changed progress bar

--- tools.py
+++ tools.py
@@ -2,8 +2,8 @@ import sys
 import shutil
 
 def updateProgressBar(percentage):
-    bar_length = shutil.get_terminal_size().columns - 10
+    bar_length = shutil.get_terminal_size().columns - 8
     blocks = int(percentage * bar_length)
-    progress_bar = "[" + "█" * blocks + "░" * (bar_length - blocks) + "]"
-    sys.stdout.write("\r" + progress_bar + f" {int(percentage * 100)}%")
+    progress_bar = "█" * blocks + "░" * (bar_length - blocks)
+    sys.stdout.write(f"\r  {int(percentage * 100)}%  {progress_bar}")
     sys.stdout.flush()
