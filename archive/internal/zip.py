f5ffa02a4472388547718928cc42fe1cf894d6d1
KokoT <prikolkillercat@gmail.com>
2024-09-24 12:54:46 +0200

    fixed compress level

--- archive/internal/zip.py
+++ archive/internal/zip.py
@@ -6,24 +6,31 @@ from tools import updateProgressBar
 
 def compress(targetPath:str, sch:dict) -> str:
     programLogger.info('zip compressing...')
+    
     zipPath = f'{targetPath}.zip'
+    compressLevel = sch.get('compressLevel', 5)
+    password = bytes(sch.get('password'), 'utf-8') if sch.get('password') else None
 
-    zfile = zipfile.ZipFile(zipPath, 'w', zipfile.ZIP_DEFLATED)
+    zfile = zipfile.ZipFile(
+        zipPath, 'w',
+        compression=zipfile.ZIP_DEFLATED, 
+        compresslevel=compressLevel
+        )
+    if password:
+        zfile.setpassword(password)
     tfile = open(targetPath, 'rb')
     tsize = os.path.getsize(targetPath) 
 
-    zip_info = zipfile.ZipInfo(os.path.basename(targetPath))
     processedSize = 0
-    with zfile.open(zip_info, 'w') as z:
-        while True:
-            data = tfile.read(COMPRESS_CHUNK_SIZE)
-            if not data:
-                break
-            
-            z.write(data)
-            
-            processedSize += len(data)
-            updateProgressBar(processedSize / tsize)
+    while True:
+        data = tfile.read(COMPRESS_CHUNK_SIZE)
+        if not data:
+            break
+        
+        zfile.writestr(os.path.basename(targetPath), data)
+        
+        processedSize += len(data)
+        updateProgressBar(processedSize / tsize)
 
     zfile.close()
     tfile.close()
