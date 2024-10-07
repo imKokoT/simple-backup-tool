96b405727f0d5e366650b5d444483aacd94aecd9
KokoT <prikolkillercat@gmail.com>
2024-10-07 13:59:43 +0200

    restoring from meta

--- restore.py
+++ restore.py
@@ -1,13 +1,32 @@
 import os
+import json
+from getpass import getpass
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
 from config import *
 import schema
 from cloud_tools import authenticate, getDestination, download
 import archiver
 import packer
 from tools import getTMP
+
+
+def tryGetMeta(service, folder:str, schemaName:str) -> dict|None:
+    programLogger.info(f'getting meta...')
+
+    query = f"'{folder}' in parents and name='{schemaName}.meta' and trashed=false"
+    response = service.files().list(q=query, spaces='drive').execute()
+    files = response.get('files', [])
+
+    if not files:
+        return False
+    
+    metaId = files[0]['id']
+    request = service.files().get_media(fileId=metaId).execute()
+
+    return json.loads(str(request,'utf-8'))
 
 
 def restore(schemaName:str, schema:dict, creds:Credentials):
@@ -24,9 +43,17 @@ def restore(schemaName:str, schema:dict, creds:Credentials):
     try:
         programLogger.info('building service')
         service = build('drive', 'v3', credentials=creds)
-
-        destinationFolder = getDestination(service, schema['destination'])
         
+        destinationFolder = getDestination(service, schema['destination'])
+
+        meta = tryGetMeta(service, destinationFolder, schemaName)
+        if meta:
+            for k, v in meta.items():
+                if k == 'password' and v and not schema.get('password'):
+                    schema['password'] = getpass(f'{YC}Archive encrypted with password; enter password: {RC}')
+                elif k != 'password':
+                    schema[k] = v
+
         programLogger.info('getting backup from cloud...')
 
         download(service, os.path.join(tmp, downloadedName), f'{schemaName}.archive', destinationFolder)
