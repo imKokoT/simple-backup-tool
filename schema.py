16aea3512c68019617c25ac3972d0d7cde87e1de
KokoT <prikolkillercat@gmail.com>
2024-10-10 10:48:55 +0200

    added opportunity to include other schemas

--- schema.py
+++ schema.py
@@ -1,5 +1,20 @@
 import os
 import yaml
+from config import *
+
+
+def include(schema:dict, include:str) -> dict:
+    t = getBackupSchema(include)
+    if not t:
+        programLogger.fatal(f'failed to include "{include}" for "{schema['__name__']}", because it not exists')
+        exit(1)
+    
+    for k, v in t.items():
+        if schema.get(k):
+            continue
+        schema[k] = v
+    
+    return schema
 
 
 def getBackupSchema(schemaName:str) ->dict|None:
@@ -11,4 +26,9 @@ def getBackupSchema(schemaName:str) ->dict|None:
         data = yaml.safe_load(f)
 
     schema = data.get(schemaName)
+    if schema:
+        schema['__name__'] = schemaName
+        if schema.get('include'):
+            return include(schema, schema.get('include'))
+
     return schema
