f1bfb580135b826f3201abd5ab967ab48f1b4081
KokoT <prikolkillercat@gmail.com>
2024-10-31 12:05:56 +0100

    refactored unpack

--- packer/unpack.py
+++ packer/unpack.py
@@ -5,26 +5,46 @@ from packer.packconfig import loadPackConfig
 from packer.tools import dumpRestoredLog, modifyRestorePaths, modifySingleRestorePath
 
 
-def invalidTargetPathHandle(path, schema, packConfig) -> str:
-    if not Config().ask_for_other_extract_path and not Config().restore_to_tmp_if_path_invalid: 
-        programLogger.error(f'failed to unpack because "{os.path.dirname(path)}" not exists')
-        return
-    elif Config().restore_to_tmp_if_path_invalid and not Config().ask_for_other_extract_path:
-        programLogger.warning(f'{os.path.dirname(path)} is invalid! files will be restored to tmp/restored')
-        path = modifySingleRestorePath(path, schema, packConfig, False)
-    else:
-        print(f'{YC}Path "{path}" is invalid, do you want to unpack to other path?')
-        newDir = getFolderPath()
-        if not newDir:
-            if not Config().restore_to_tmp_if_path_invalid:
-                programLogger.info(f'skipped')
-                return
-            
-            path = modifySingleRestorePath(path, schema, packConfig, False)
-        else:
-            path = os.path.join(newDir.strip(), os.path.basename(path))
+def unpackAll(schemaName:str, schema:dict):
+    programLogger.info('unpacking process started')
+   
+    tmp = getTMP()
+    os.makedirs(f'{tmp}/restored/{schemaName}', exist_ok=True)
+    archive = tarfile.open(os.path.join(tmp, f'{schemaName}.tar'), 'r')
 
-    return path
+    packConfig = loadPackConfig(archive)
+
+    dumpRestoredLog(packConfig, schema)
+    
+    if Config().allow_local_replace and Config().ask_before_replace:
+        askForLocalReplace(schemaName, schema, packConfig)
+
+    result = {
+        'rewritten': 0,
+        'restored': 0
+    }
+    res:dict
+    i = 0
+    for folder in packConfig['folders']:
+        res = unpackFolder(folder, i, archive, schema, packConfig)
+        
+        if res:
+            result['rewritten'] += res['rewritten']
+            result['restored'] += res['restored']
+        i += 1
+    i = 0
+    for file in packConfig['files']:
+        res = unpackFile(file, i, archive, schema, packConfig)
+
+        if res:
+            result['rewritten'] += res['rewritten']
+            result['restored'] += res['restored']
+        i += 1
+
+    programLogger.info(f'unpacking finished with success!\n'
+                       f' - rewritten total: {result['rewritten']}\n'
+                       f' - restored total: {result["restored"]}')
+    archive.close()
 
 
 def unpackFile(path:str, index:int, archive:tarfile.TarFile, schema:dict, packConfig:dict):
@@ -96,57 +116,43 @@ def unpackFolder(path:str, index:int, archive:tarfile.TarFile, schema:dict, pack
     }
 
 
-def unpackAll(schemaName:str, schema:dict):
-    programLogger.info('unpacking process started')
-   
+def askForLocalReplace(schemaName, schema, packConfig):
     tmp = getTMP()
-    os.makedirs(f'{tmp}/restored/{schemaName}', exist_ok=True)
-    archive = tarfile.open(os.path.join(tmp, f'{schemaName}.tar'), 'r')
 
-    packConfig = loadPackConfig(archive)
-
-    dumpRestoredLog(packConfig, schema)
-    
-    if Config().allow_local_replace and Config().ask_before_replace:
-        print(f'{LYC}Are you sure to rewrite next folders and files:')
-        for f in packConfig['folders']:
-            print(f'{LYC} - {f}')
-        for f in packConfig['files']:
-            print(f'{LYC} - {f}')
+    print(f'{LYC}Are you sure to rewrite next folders and files:')
+    for f in packConfig['folders']:
+        print(f'{LYC} - {f}')
+    for f in packConfig['files']:
+        print(f'{LYC} - {f}')
+    yn = input('[y/N] ')
+    if yn.strip().lower() != 'y':
+        print('restored data placed in tmp folder')
+        print(f'{LYC}do you want to unpack all to "{tmp}/restored/{schemaName}":')
         yn = input('[y/N] ')
-        if yn.strip().lower() != 'y':
-            print('restored data placed in tmp folder')
-            print(f'{LYC}do you want to unpack all to "{tmp}/restored/{schemaName}":')
-            yn = input('[y/N] ')
-            if yn.strip().lower() == 'y':
-                modifyRestorePaths(packConfig, schema)
-            else:
-                print('unpack process interrupted...')
-                exit(0)
+        if yn.strip().lower() == 'y':
+            modifyRestorePaths(packConfig, schema)
+        else:
+            print('unpack process interrupted...')
+            exit(0)
 
-    result = {
-        'rewritten': 0,
-        'restored': 0
-    }
-    res:dict
-    i = 0
-    for folder in packConfig['folders']:
-        res = unpackFolder(folder, i, archive, schema, packConfig)
-        
-        if res:
-            result['rewritten'] += res['rewritten']
-            result['restored'] += res['restored']
-        i += 1
-    i = 0
-    for file in packConfig['files']:
-        res = unpackFile(file, i, archive, schema, packConfig)
 
-        if res:
-            result['rewritten'] += res['rewritten']
-            result['restored'] += res['restored']
-        i += 1
+def invalidTargetPathHandle(path, schema, packConfig) -> str:
+    if not Config().ask_for_other_extract_path and not Config().restore_to_tmp_if_path_invalid: 
+        programLogger.error(f'failed to unpack because "{os.path.dirname(path)}" not exists')
+        return
+    elif Config().restore_to_tmp_if_path_invalid and not Config().ask_for_other_extract_path:
+        programLogger.warning(f'{os.path.dirname(path)} is invalid! files will be restored to tmp/restored')
+        path = modifySingleRestorePath(path, schema, packConfig, False)
+    else:
+        print(f'{YC}Path "{path}" is invalid, do you want to unpack to other path?')
+        newDir = getFolderPath()
+        if not newDir:
+            if not Config().restore_to_tmp_if_path_invalid:
+                programLogger.info(f'skipped')
+                return
+            
+            path = modifySingleRestorePath(path, schema, packConfig, False)
+        else:
+            path = os.path.join(newDir.strip(), os.path.basename(path))
 
-    programLogger.info(f'unpacking finished with success!\n'
-                       f' - rewritten total: {result['rewritten']}\n'
-                       f' - restored total: {result["restored"]}')
-    archive.close()
+    return path
