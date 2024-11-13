b5170f6554255b589affa7610f8b7d2b5359e7db
KokoT <prikolkillercat@gmail.com>
2024-11-13 14:24:05 +0100

    added warning if in config detected unknown param

--- config.py
+++ config.py
@@ -60,6 +60,8 @@ class Config:
                 config = yaml.safe_load(f)
                 for k, v in config.items():
                     setattr(Config(), k, v)
+                    if k not in Config.__dict__.keys():
+                        logger.warning(f'detected unknown parameter "{k}" in config.yaml')
                 if config != Config().__dict__:
                     logger.info('updated config.yaml to newer version')
                     Config.save()
