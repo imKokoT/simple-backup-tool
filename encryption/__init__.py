37ef5f03767576c5a8937bcd520a5b7b35ceb698
KokoT <prikolkillercat@gmail.com>
2025-01-22 20:35:49 +0100

    added: - base encryption structure - key generation from password and genbytes functions

---
+++ encryption/__init__.py
@@ -0,0 +1,10 @@
+
+try:
+    import cryptography as _
+except ModuleNotFoundError:
+    from logger import logger
+    logger.debug(f'encryption disabled; module "cryptography" not installed!')
+else:
+    from . import keygen
+    from . import aes
+    from . import chacha20
---
+++ encryption/keygen.py
@@ -0,0 +1,22 @@
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+import os
+
+KEY_SIZE = 32
+ITERATIONS = 100_000
+
+
+def keygen(password:str, salt:bytes) -> bytes:
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=KEY_SIZE,
+        salt=salt,
+        iterations=ITERATIONS,
+        backend=default_backend()
+    )
+    return kdf.derive(password.encode())
+
+
+def bytesgen(size:int) -> bytes:
+    return os.urandom(size)
