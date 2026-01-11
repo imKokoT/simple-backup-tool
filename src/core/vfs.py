58a18593872bbe0bd1ebc664984c0aa65211c275
KokoT <prikolkillercat@gmail.com>
2026-01-11 16:00:38 +0100

    conception of virtual file system #15

---
+++ src/core/vfs.py
@@ -0,0 +1,55 @@
+from io import BytesIO, FileIO, IOBase
+from pathlib import Path
+from typing import Literal
+
+
+class VirtualFS:
+    """Virtual filesystem to manage virtual files"""
+    _vfs:dict[Path, 'VFile'] = {}
+
+    def get(self, path:Path) -> 'VFile | None':
+        return self._vfs.get(path)
+
+    def _on_close(self, path:Path):
+        self._vfs.pop(path)
+
+    def _on_open(self, vfile:'VFile'):
+        if vfile._path in self._vfs:
+            raise FileExistsError(f'virtual file "{vfile._path}" already exists')
+        self._vfs[vfile._path] = vfile
+
+vfs = VirtualFS()
+
+
+class VFile(IOBase):
+    """Virtual file IO that can be stored in RAM or disk"""
+    def __init__(self, path:Path, mode:str='r', data:bytes=b'', location:Literal['ram', 'disk']|None=None):
+        if location is None:
+            raise NotImplementedError() # TODO app configurations of location
+        elif location == 'ram':
+            self._file = BytesIO(data)
+        elif location == 'disk':
+            self._file = FileIO(path, mode)
+            if data:
+                self._file.write(data)
+        else:
+            raise ValueError(f'Unknown storage location "{location}"')
+        
+        self._path = path
+        self._mode = mode
+        self._location = location
+        
+        vfs._on_open(self)
+
+    def read(self, n: int = -1) -> bytes:                return self._file.read(n)
+    def write(self, b: bytes) -> int:                    return self._file.write(b)
+    def seek(self, offset: int, whence: int = 0) -> int: return self._file.seek(offset, whence)
+    def tell(self) -> int:                               return self._file.tell()
+
+    def close(self):
+        if not self._file.closed:
+            self._file.close()
+            vfs._on_close(self._path)
+
+    def __getattr__(self, name):
+        return getattr(self._file, name)
